f499dbe605cf3a17ac25a80ada538da4d5448b56
KEVIN3349 <KEVIN3349@PCM-70KFP13>
2022-07-06 19:17:59 +0800

    Implemented the same logic to resource group approach

--- InstallMMAByResourceGroup.ps1
+++ InstallMMAByResourceGroup.ps1
@@ -6,39 +6,87 @@ param(
     [bool] $shouldReplaceExisting = $false
 )
 
-function GetVirtualMachinesByResourceGroup
+function ValidateVirtualMachines
 {
     $virtualMachines = az vm list --resource-group $resourceGroup --query "[?powerState=='VM running']" -d -o json | ConvertFrom-Json
+    
+    if ($null -eq $virtualMachines)
+    {
+        Write-Error "Resource Group: $resourceGroup does not exist or does not have a running virtual machine"
+        exit 1
+    }
+
     return $virtualMachines
 }
 
-function UpdateVirtualMachineAgents
+function ListVirtualMachineWorkspaces
 {
     param(
-        [object] $virtualMachines
+        [string] $virtualMachineName
     )
 
-    foreach ($virtualMachine in $virtualMachines)
+    $getWorkspaces = az vm run-command invoke --command-id RunPowerShellScript `
+    --name $virtualMachineName `
+    --resource-group $resourceGroup `
+    --scripts "@run-commands/GetWorkspacesFromVirtualMachine.ps1" | ConvertFrom-Json
+
+    $workspaceIdList = $getWorkspaces.value[0].message.Split()
+
+    return $workspaceIdList
+}
+
+function UpdateVirtualMachineWorkspaces
+{
+    param(
+        [string] $virtualMachineName,
+        [string[]] $workspaceIdList
+    )
+
+    if ($workspaceIdList.Count -eq 4 -and !$shouldReplaceExisting) 
     {
-        foreach ($resource in $virtualMachine.resources)
+        Write-Error "Virtual Machine: $virtualMachineName has four (4) workspaces already"
+        exit 1
+    }
+
+    if ($workspaceIdList.Count -gt 0)
+    {
+        foreach ($id in $workspaceIdList)
         {
-            if($shouldReplaceExisting -and $resource.typePropertiesType -eq "MicrosoftMonitoringAgent")
+            if ($shouldReplaceExisting)
             {
-                az vm extension delete -g $virtualMachine.resourceGroup --vm-name $virtualMachine.name -n $resource.name
+                az vm run-command invoke --command-id RunPowerShellScript `
+                --name $virtualMachineName `
+                --resource-group $resourceGroup `
+                --scripts "@run-commands/RemoveWorkspaceOnVirtualMachine.ps1" `
+                --parameters "workspaceId=$id"
+            }
+
+            elseif ($id -eq $workspaceId -and !$shouldReplaceExisting)
+            {
+                Write-Error "Workspace ID: $workspaceId is already connected to Virtual Machine: $virtualMachineName"
+                exit 1
             }
         }
-
-        az vm run-command invoke --command-id RunPowerShellScript --name $virtualMachine.name --resource-group $resourceGroup --scripts "@AddWorkspaceOnVirtualMachine.ps1" --parameters "workspaceId=$workspaceId" "workspaceKey=$workspaceKey"
     }
+
+    az vm run-command invoke --command-id RunPowerShellScript `
+    --name $virtualMachineName `
+    --resource-group $resourceGroup `
+    --scripts "@run-commands/AddWorkspaceOnVirtualMachine.ps1" `
+    --parameters "workspaceId=$workspaceId" "workspaceKey=$workspaceKey"
 }
 
 try
 {
     az account set --subscription $subscription
 
-    $virtualMachines = GetVirtualMachinesByResourceGroup
+    $virtualMachines = ValidateVirtualMachines
     
-    UpdateVirtualMachineAgents $virtualMachines
+    foreach ($virtualMachine in $virtualMachines)
+    {
+        $workspaceIdList = ListVirtualMachineWorkspaces $virtualMachine.name
+        UpdateVirtualMachineWorkspaces $virtualMachine.name $workspaceIdList
+    }
 }
 
 catch 
