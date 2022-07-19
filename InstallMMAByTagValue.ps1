d5810f3ca3dff4f0c8011134f54ee8eb401973cb
KEVIN3349 <KEVIN3349@PCM-70KFP13>
2022-07-19 19:04:37 +0800

    Added conditions

---
+++ InstallMMAByTagValue.ps1
@@ -0,0 +1,104 @@
+param(
+    [string] $subscription,
+    [string] $resourceGroup,
+    [string] $tagValue,
+    [string] $workspaceId,
+    [string] $workspaceKey,
+    [bool] $shouldReplaceExisting = $false
+)
+
+function ValidateVirtualMachines
+{
+    $virtualMachines = az vm list --query "[?contains(storageProfile.osDisk.osType, 'Windows') && tags.Onboarding == '$tagValue' && powerState=='VM running']" -d -o json | ConvertFrom-Json
+    
+    if ($null -eq $virtualMachines)
+    {
+        Write-Error "Tag Value: $tagValue does not exist or does not have a running virtual machine"
+        exit 1
+    }
+
+    return $virtualMachines
+}
+
+function ListVirtualMachineWorkspaces
+{
+    param(
+        [string] $virtualMachineName
+    )
+
+    $getWorkspaces = az vm run-command invoke --command-id RunPowerShellScript `
+    --name $virtualMachineName `
+    --resource-group $resourceGroup `
+    --scripts "@run-commands/GetWorkspacesFromVirtualMachine.ps1" | ConvertFrom-Json
+
+    $workspaceIdList = $getWorkspaces.value[0].message.Split()
+
+    return $workspaceIdList
+}
+
+function UpdateVirtualMachineWorkspaces
+{
+    param(
+        [string] $virtualMachineName,
+        [string[]] $workspaceIdList
+    )
+
+    $shouldAddWorkspace = $true
+
+    if ($workspaceIdList.Count -eq 4 -and !$shouldReplaceExisting) 
+    {
+        Write-Error "Virtual Machine: $virtualMachineName has four (4) workspaces already"
+        break
+    }
+
+    if ($workspaceIdList.Count -gt 0)
+    {
+        foreach ($id in $workspaceIdList)
+        {
+            if ($shouldReplaceExisting)
+            {
+                az vm run-command invoke --command-id RunPowerShellScript `
+                --name $virtualMachineName `
+                --resource-group $resourceGroup `
+                --scripts "@run-commands/RemoveWorkspaceOnVirtualMachine.ps1" `
+                --parameters "workspaceId=$id"
+            }
+
+            elseif ($id -eq $workspaceId -and !$shouldReplaceExisting)
+            {
+                $shouldAddWorkspace = $false
+                Write-Output "Workspace ID: $workspaceId is already connected to Virtual Machine: $virtualMachineName"
+                break
+            }
+        }
+    }
+
+    if ($workspaceIdList.Count -lt 4 -and $shouldAddWorkspace)
+    {
+        az vm run-command invoke --command-id RunPowerShellScript `
+        --name $virtualMachineName `
+        --resource-group $resourceGroup `
+        --scripts "@run-commands/AddWorkspaceOnVirtualMachine.ps1" `
+        --parameters "workspaceId=$workspaceId" "workspaceKey=$workspaceKey"
+    }
+}
+
+try
+{
+    az account set --subscription $subscription
+
+    $virtualMachines = ValidateVirtualMachines
+    
+    foreach ($virtualMachine in $virtualMachines)
+    {
+        $workspaceIdList = ListVirtualMachineWorkspaces $virtualMachine.name
+        UpdateVirtualMachineWorkspaces $virtualMachine.name $workspaceIdList
+    }
+
+    Write-Output "Done running the script"
+}
+
+catch 
+{
+    Write-Output $_
+}
