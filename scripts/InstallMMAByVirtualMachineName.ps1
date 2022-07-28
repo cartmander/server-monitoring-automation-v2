622470ef942a7eb3f37205194d1234d28f94cf67
KEVIN3349 <KEVIN3349@PCM-70KFP13>
2022-07-28 15:36:32 +0800

    Reverted array splatting

--- scripts/InstallMMAByVirtualMachineName.ps1
+++ scripts/InstallMMAByVirtualMachineName.ps1
@@ -28,12 +28,11 @@ function ValidateVirtualMachine
 
 function ListVirtualMachineWorkspaces
 {
-    $params = "--command-id", "RunPowerShellScript", 
-    "--name", $virtualMachineName, 
-    "--resource-group", $resourceGroup, 
-    "--scripts", "C:\scripts\GetWorkspacesFromVirtualMachine.ps1"
+    $getWorkspaces = az vm run-command invoke `
+    --command-id RunPowerShellScript `
+    --name $virtualMachineName --resource-group $resourceGroup `
+    --scripts "C:\scripts\GetWorkspacesFromVirtualMachine.ps1" | ConvertFrom-Json
 
-    $getWorkspaces = az vm run-command invoke $params | ConvertFrom-Json
     $workspaceIdList = $getWorkspaces.value[0].message.Split()
 
     return $workspaceIdList
@@ -69,13 +68,12 @@ function UpdateVirtualMachineWorkspaces
 
     if ($workspaceIdList.Count -lt 4 -and $shouldAddWorkspace)
     {
-        $params = "--command-id", "RunPowerShellScript", 
-        "--name", $virtualMachineName, 
-        "--resource-group", $resourceGroup, 
-        "--scripts", "C:\scripts\AddWorkspaceOnVirtualMachine.ps1", 
-        "--parameters", "'workspaceId=$workspaceId' 'workspaceKey=$workspaceKey'"
-
-        az vm run-command invoke $params
+        az vm run-command invoke `
+        --command-id RunPowerShellScript `
+        --name $virtualMachineName `
+        --resource-group $resourceGroup `
+        --scripts "C:\scripts\AddWorkspaceOnVirtualMachine.ps1" `
+        --parameters "workspaceId=$workspaceId" "workspaceKey=$workspaceKey"
     }
 }
 
