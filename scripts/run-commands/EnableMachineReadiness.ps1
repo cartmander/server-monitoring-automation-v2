6ed29873065384f966b7f4e1fd0321e057230702
KEVIN3349 <KEVIN3349@PCM-70KFP13>
2022-08-12 17:58:35 +0800

    Formatting of machine readiness

--- scripts/run-commands/EnableMachineReadiness.ps1
+++ scripts/run-commands/EnableMachineReadiness.ps1
@@ -1,35 +1,47 @@
-begin {
-    try {
+begin 
+{
+    try 
+    {
         Stop-Service HealthService -Verbose
     }
-    catch {
+
+    catch 
+    {
         Write-Error "$($error[0].Exception.Message)"
         Write-Host "Stopping script. We need to stop the HealthService properly."
         Break
     }
 
     $StoppingServiceTime = (Get-Date).AddSeconds(30)
-    do {
+
+    do 
+    {
         Write-Host "Waiting for Service to be stopped completely before continuing"
         $ServiceStatus = (Get-Service -Name HealthService).Status
-    } until ($ServiceStatus -eq "Stopped" -or (New-TimeSpan -End $StoppingServiceTime))
+    } 
+    until ($ServiceStatus -eq "Stopped" -or (New-TimeSpan -End $StoppingServiceTime))
 
-    Write-Host "HealtService is now $ServiceStatus, SCOM monitoring will be stopped as well." -ForegroundColor Yellow
+    Write-Host "HealthService is now $ServiceStatus, SCOM monitoring will be stopped as well." -ForegroundColor Yellow
 }
 
-process {
-    Try {
+process
+{
+    try 
+    {
         Remove-Item `
             -path "HKLM:\SOFTWARE\Microsoft\HybridRunbookWorker" `
             -Recurse `
             -Force `
             -ErrorAction Stop
     }
-    catch { 
+
+    catch 
+    { 
         Write-Warning "$($error[0].Exception.Message)"
     }
 
-    Try {
+    try 
+    {
 
         $currentuser = whoami.exe
         $acl = Get-Acl "C:\Program Files\Microsoft Monitoring Agent\Agent\Health Service State"
@@ -44,21 +56,27 @@ process {
             -Force `
             -ErrorAction Stop
     }
-    catch {
+
+    catch 
+    {
         Write-Warning "$($error[0].Exception.Message)"
     }
 
     $StartingServiceTime = (Get-Date).AddSeconds(30)
     Start-Service HealthService
-    do {
+
+    do 
+    {
         Write-Host "Waiting for HealthService to be started completely before continuing"
         $StartedServiceStatus = (Get-Service -Name HealthService).Status
-    } until ($StartedServiceStatus -eq "Running" -or (New-TimeSpan -End $StartingServiceTime))
+    } 
+    until ($StartedServiceStatus -eq "Running" -or (New-TimeSpan -End $StartingServiceTime))
 
-    Write-Host "HealtService is now $($StartedServiceStatus) , SCOM monitoring will be resumed as well." -ForegroundColor Green
+    Write-Host "HealthService is now $($StartedServiceStatus) , SCOM monitoring will be resumed as well." -ForegroundColor Green
 }
 
 
-end {
+end 
+{
     Write-Host "Script completed. Please check the Azure Automation account SYSTEM Hybrid workers" -ForegroundColor Green
 }
