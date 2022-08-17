d75ff8ef3578b2882045e6712f1a5d12b2a7ac36
KEVIN3349 <KEVIN3349@PCM-70KFP13>
2022-08-17 10:51:43 +0800

    Try catches

--- scripts/run-commands/OnboardVirtualMachine.ps1
+++ scripts/run-commands/OnboardVirtualMachine.ps1
@@ -32,26 +32,43 @@ begin
 
 process
 {
+    try
+    {
     # Add Workspace on Virtual Machine
     $agent = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
     $agent.AddCloudWorkspace($workspaceId, $workspaceKey)
     $agent.ReloadConfiguration()
-
-    # Remove From SCCM Collection
-    $Today = [datetime]::Today.ToString('MM/dd/yyyy')
-    $WTWRegPath = "HKLM:\SOFTWARE\WTW\Patching"
-    $PatchSchd = "AZR-UPD-MGR"
-    $RequestedBy = "Cloud Ops Team"
-
-    if (!(Test-Path $WTWRegPath)) {
-        New-Item -Name "WTW" -Path 'HKLM:\SOFTWARE\' -type Directory
-        New-Item -Name "Patching" -Path 'HKLM:\SOFTWARE\WTW' -type Directory
     }
 
-    Set-ItemProperty -Path "$WTWRegPath" -Name Patchschd -Value $PatchSchd
-    Set-ItemProperty -Path "$WTWRegPath" -Name RequestDate -Value $Today
-    Set-ItemProperty -Path "$WTWRegPath" -Name RequestedBy -Value $RequestedBy
-    Set-ItemProperty -Path "$WTWRegPath" -Name ChangeStamp -Value $Today
+    catch
+    {
+        Write-Warning "$($error[0].Exception.Message)"
+    }
+
+    try
+    {
+        # Remove From SCCM Collection
+        $Today = [datetime]::Today.ToString('MM/dd/yyyy')
+        $WTWRegPath = "HKLM:\SOFTWARE\WTW\Patching"
+        $PatchSchd = "AZR-UPD-MGR"
+        $RequestedBy = "Cloud Ops Team"
+
+        if (!(Test-Path $WTWRegPath)) {
+            New-Item -Name "WTW" -Path 'HKLM:\SOFTWARE\' -type Directory
+            New-Item -Name "Patching" -Path 'HKLM:\SOFTWARE\WTW' -type Directory
+        }
+
+        Set-ItemProperty -Path "$WTWRegPath" -Name Patchschd -Value $PatchSchd
+        Set-ItemProperty -Path "$WTWRegPath" -Name RequestDate -Value $Today
+        Set-ItemProperty -Path "$WTWRegPath" -Name RequestedBy -Value $RequestedBy
+        Set-ItemProperty -Path "$WTWRegPath" -Name ChangeStamp -Value $Today
+    }
+
+    catch
+    {
+        Write-Warning "$($error[0].Exception.Message)"
+    }
+
 
     try 
     {
