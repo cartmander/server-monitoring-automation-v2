1b0276d8f4ffa73002da60470e7e6b8330bd614b
KurtRanzel.Flores <kurtranzel.flores@willistowerswatson.com>
2022-08-24 06:09:19 +0800

    Removed counter output

--- scripts/MonitoringAgentInstallation.ps1
+++ scripts/MonitoringAgentInstallation.ps1
@@ -92,9 +92,6 @@ try
 
     $virtualMachine = ValidateVirtualMachine
     $virtualMachineName = $virtualMachine.name
-
-    Write-Host "Onboarding Virtual Machine(s): $virtualMachineName [$currentCount of $total]..." -ForegroundColor Cyan
-    
     $workspaceIdList = ListVirtualMachineWorkspaces $virtualMachineName
     UpdateVirtualMachineWorkspaces $virtualMachineName $workspaceIdList
 }
