92b23486c8f6f08739a9e3fa213414756e6bde27
KEVIN3349 <KEVIN3349@PCM-70KFP13>
2022-08-29 22:08:56 +0800

    Fixed bracketing

--- scripts/ProcessCsvData.ps1
+++ scripts/ProcessCsvData.ps1
@@ -9,30 +9,28 @@ param(
 function VerifyJobState
 {
     param(
-        [object] $ChildJobs
+        [object] $_
     )
 
-    $ChildJobs | ForEach-Object -Process 
-    {
-        Write-Host "=================================================="
-        Write-Host "Job output for $($_.Name)"
-        Write-Host "=================================================="
+    Write-Host "=================================================="
+    Write-Host "Job output for $($_.Name)"
+    Write-Host "=================================================="
 
-        $_ | Receive-Job -Keep
-        if ($state -eq "Completed") 
-        {
-            Write-Host "$($_.Name) finished executing with `"$($_.State)`" state" -ForegroundColor Green
-        }
+    $_ | Receive-Job -Keep
     
-        elseif ($_.State -eq "Stopped") 
-        {
-            Write-Host "$($_.Name) finished executing with `"$($_.State)`" state" -ForegroundColor Red
-        }
-        
-        else 
-        {
-            Write-Host "$($_.Name.Replace('ChildJob','')) finished executing with `"$($_.State)`" state" -ForegroundColor Yellow
-        }
+    if ($_.State -eq "Completed") 
+    {
+        Write-Host "$($_.Name) finished executing with `"$($_.State)`" state" -ForegroundColor Green
+    }
+
+    elseif ($_.State -eq "Stopped") 
+    {
+        Write-Host "$($_.Name) finished executing with `"$($_.State)`" state" -ForegroundColor Red
+    }
+    
+    else 
+    {
+        Write-Host "$($_.Name.Replace('ChildJob','')) finished executing with `"$($_.State)`" state" -ForegroundColor Yellow
     }
 }
 
@@ -41,16 +39,16 @@ function JobLogging
     Write-Host "Waiting for jobs to finish executing..."
 
     $JobTable = Get-Job | Wait-Job | Where-Object {$_.Name -like "*OnboardingJob"}
-    $JobTable | ForEach-Object -Process 
-    {
+    $JobTable | ForEach-Object -Process {
         $_.ChildJobs[0].Name = $_.Name.Replace("OnboardingJob", "ChildJob")
     }
 
     $ChildJobs = Get-Job -IncludeChildJob | Where-Object {$_.Name -like "*ChildJob"}
+    $ChildJobs | ForEach-Object -Process {
+        VerifyJobState $_
+    }
 
-    VerifyJobState $ChildJobs
-
-    $ChildJobs | Select-Object -Property Id, Name, State, PSBeginTime, PSEndTime | Format-Table
+    $ChildJobs | Select-Object -Property Id,Name, State, PSBeginTime,PSEndTime|Format-Table
 }
 
 function ValidateCsv
@@ -81,8 +79,7 @@ try
     $csv = Import-Csv ".\csv\VirtualMachines.csv"
     ValidateCsv $csv
 
-    $csv | ForEach-Object -Process 
-    {
+    $csv | ForEach-Object -Process {
         $MMAInstallationParameters = @(
             $_.Subscription
             $_.ResourceGroup
