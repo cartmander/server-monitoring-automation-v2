f493a2276f64cfec8b4c149d24953ae3d25e5b3f
KEVIN3349 <KEVIN3349@PCM-70KFP13>
2022-08-29 21:40:27 +0800

    Reverted changes

--- scripts/ProcessCsvData.ps1
+++ scripts/ProcessCsvData.ps1
@@ -5,54 +5,6 @@ param(
     [Parameter(Mandatory=$true)]
     [string] $password
 )
-
-function VerifyJobState
-{
-    param(
-        [object] $_
-    )
-
-    Write-Host "=================================================="
-    Write-Host "Job output for $($_.Name)"
-    Write-Host "=================================================="
-
-    $_ | Receive-Job -Keep
-    
-    if ($_.State -eq "Completed") 
-    {
-        Write-Host "$($_.Name) finished executing with `"$($_.State)`" state" -ForegroundColor Green
-    }
-
-    elseif ($_.State -eq "Stopped") 
-    {
-        Write-Host "$($_.Name) finished executing with `"$($_.State)`" state" -ForegroundColor Red
-    }
-    
-    else 
-    {
-        Write-Host "$($_.Name.Replace('ChildJob','')) finished executing with `"$($_.State)`" state" -ForegroundColor Yellow
-    }
-}
-
-function JobLogging 
-{
-    Write-Host "Waiting for jobs to finish executing..."
-
-    $JobTable = Get-Job | Wait-Job | Where-Object {$_.Name -like "*OnboardingJob"}
-    $JobTable | ForEach-Object -Process 
-    {
-        $_.ChildJobs[0].Name = $_.Name.Replace("OnboardingJob", "ChildJob")
-    }
-
-    $ChildJobs = Get-Job -IncludeChildJob | Where-Object {$_.Name -like "*ChildJob"}
-    $ChildJobs | ForEach-Object -Process 
-    {
-        VerifyJobState $_
-    }
-
-    $ChildJobs | Select-Object -Property Id,Name, State, PSBeginTime,PSEndTime|Format-Table
-}
-
 function ValidateCsv
 {
     param(
@@ -72,17 +24,39 @@ function ValidateCsv
     }
 }
 
-try 
-{
-    az login -u $username -p $password
+function JobLogging {
+    Write-Host "Waiting for jobs to finish executing..."
+    $JobTable = Get-Job | Wait-Job | Where-Object {$_.Name -like "*OnboardingJob"}
+    $JobTable | ForEach-Object -Process {
+        $_.ChildJobs[0].Name = $_.Name.Replace("OnboardingJob","ChildJob")
+    }
+    $ChildJobs = Get-Job -IncludeChildJob | Where-Object {$_.Name -like "*ChildJob"}
+    $ChildJobs | ForEach-Object -Process {
+        Write-Host "=================================================="
+        Write-Host "Job output for $($_.Name)"
+        Write-Host "=================================================="
+        $_ | Receive-Job -Keep
+        if ($_.State -eq "Completed") {
+            Write-Host "$($_.Name) finished executing with `"$($_.State)`" state" -ForegroundColor Green
+        }
+        elseif ($_.State -eq "Stopped") {
+            Write-Host "$($_.Name) finished executing with `"$($_.State)`" state" -ForegroundColor Red
+        }
+        else {
+            Write-Host "$($_.Name.Replace('ChildJob','')) finished executing with `"$($_.State)`" state" -ForegroundColor Yellow
+        }
+    }
+    $ChildJobs | Select-Object -Property Id,Name, State, PSBeginTime,PSEndTime|Format-Table
+}
 
+
+
+try {
+    az login -u $username -p $password
     Write-Host "Initializing automation..." -ForegroundColor Green
-    
     $csv = Import-Csv ".\csv\VirtualMachines.csv"
     ValidateCsv $csv
-
-    $csv | ForEach-Object -Process 
-    {
+    $csv | ForEach-Object -Process {
         $MMAInstallationParameters = @(
             $_.Subscription
             $_.ResourceGroup
@@ -93,13 +67,10 @@ try
         Start-Job -Name "$($_.VirtualMachineName)OnboardingJob" -ErrorAction Stop -FilePath .\scripts\MonitoringAgentInstallation.ps1 -ArgumentList $MMAInstallationParameters
         #Logging Function TODO: Improvements
     }
-
     JobLogging
-
     Write-Host "Done running the automation..." -ForegroundColor Green
 }
-catch 
-{
+catch {
     Write-Host $_
     exit $LASTEXITCODE
 }
